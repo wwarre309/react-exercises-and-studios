099403ad45fa177eeb5b3feea953cade056cdcd6
Sally Steuterman <sally.steuterman@gmail.com>
2023-05-02 11:50:24 -0500

    Set up component for assigning recipe to board

---
+++ part3/studio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoardAssignment from './components/BoardAssignment';
+
+function App() {
+  return (
+    <div className="App">
+      <BoardAssignment />
+    </div>
+  );
+}
+
+export default App;
