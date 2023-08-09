9f1b498df56815a6d75809e0c84509da82b32d12
Will <138933808+wwarre309@users.noreply.github.com>
2023-08-09 16:13:01 -0400

    practice

---
+++ practice/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ShoeButton from './components/ShoeButton';
+
+function App() {
+  return (
+    <ShoeButton />
+    
+  );
+
+}
+
+export default App;
