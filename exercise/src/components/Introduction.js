34484b4f1b97c4cc26ee7773fdfe439d7c12c6b1
Will <138933808+wwarre309@users.noreply.github.com>
2023-08-10 09:26:26 -0400

    saving

---
+++ exercise/src/components/Introduction.js
@@ -0,0 +1,13 @@
+export default function HobbyIntroduction () {
+
+    return(
+        <div>
+            <h2>Fun Facts About Basketball</h2>
+            <ol>
+                <li>Basketball was created in 1891 by James Naismith</li>
+                <li>Basketball season consists of 82 regular season games</li>
+                <li>Basketball courts are 94ft long and 50ft wide</li>
+            </ol>
+        </div>
+    );
+}
---
+++ exercise/src/components/Projects.js
@@ -0,0 +1,7 @@
+import data from '../data.json'
+import { useState } from 'react'
+ 
+
+export default function MyProjects () {
+    const [index, setIndex] = useState(0);
+}
