c95aad860a1f668cf594257b12beb93fc21f7e51
Will <138933808+wwarre309@users.noreply.github.com>
2023-08-10 16:25:57 -0400

    Studio Done

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import HobbyIntroduction from './components/Introduction';
+import HobbyIntroduction from './components/Introduction.js';
+import MyProjects from './components/Projects.js'
 
 function App() {
   return (
     <div className="App">
       <h1>My Hobby: Basketball</h1>
       <HobbyIntroduction/> 
+      <MyProjects />
     </div>
   );
 }
--- exercise/src/components/Projects.js
+++ exercise/src/components/Projects.js
@@ -1,7 +1,37 @@
-import data from '../data.json'
+import teamData from '../data.json'
 import { useState } from 'react'
  
 
 export default function MyProjects () {
     const [index, setIndex] = useState(0);
+    let projects = teamData.projects[index]
+
+
+    function handleClick() {
+        if (index < teamData.projects.length -1)
+    {
+        setIndex(index + 1);
+    } else {
+        setIndex(0);
+    }
+    
+}
+
+    return (
+        
+        <div>
+        <button id="clickedButton" onClick={handleClick}>
+            Next
+        </button>
+        
+        <h3> Team Name: {projects.Team}</h3>
+        <ul> 
+            <li>Team City: {projects.Location}</li>
+           
+            <li>Team Division: {projects.Division}</li>
+            <li> <img className="logo" src={projects.Logo}></img></li>
+        </ul>
+        </div>
+
+    )
 }
