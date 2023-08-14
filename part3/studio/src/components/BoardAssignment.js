866300d702e78a197280df1dc803259f159e2530
Will <138933808+wwarre309@users.noreply.github.com>
2023-08-14 10:44:55 -0400

    done

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -21,22 +21,28 @@ export default function BoardAssignment () {
 
    ];
 
-   const mappedBoards = boards.map({label, value})
+   const [boardName, setBoardName] = useState('no boards yet')
       
-      <select>
-         <option value={boards.value}> {boards.label}</option>
-      </select>
+      // <select>
+      //    <option value={boards.value}> {boards.label}</option>
+      // </select>
 
    const handleChange = (event) => {
+      setBoardName(event.target.value)
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
-      {/* <select value={boardName} onChange={handleChange}>
-      </select> */}
-
-      {/* <p>Saved to {boardName}!</p> */}
+      <select value={boardName} onChange={handleChange}>
+      
+      {boards.map((board) => (
+      <option value={boards.value}> {boards.label}</option>
+      ))}
+      </select> 
+   
+       <p>Saved to {boardName}!</p>
       </div>
    );
+   
 }
